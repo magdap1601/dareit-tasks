66bae9759bff185b05eb15bdb31d7c7f8a435d6e
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-03-28 20:52:29 +0200

    feat: data createdy by terraform

---
+++ task_6/main.tf
@@ -0,0 +1,59 @@
+provider "google" {
+ project = "evident-zone-377721"
+ region = "us-central1"
+ zone  = "us-central1-c"
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+ bucket = google_storage_bucket.bucket.name
+ role  = "READER"
+ entity = "allUsers"
+}
+
+resource "google_storage_bucket" "bucket" {
+ name   = "bucket-created-by-terraform"
+ location = "US"
+}
+
+resource "google_compute_instance" "dare-id-vm" {
+ name     = "dareit-vm-tf"
+ machine_type = "e2-medium"
+ zone     = "us-central1-a"
+
+ tags = ["dareit"]
+
+ boot_disk {
+  initialize_params {
+   image = "debian-cloud/debian-11"
+   labels = {
+    managed_by_terraform = "true"
+   }
+  }
+ }
+
+ network_interface {
+  network = "default"
+
+  access_config {
+   // Ephemeral public IP
+  }
+ }
+}
+
+resource "google_sql_database_instance" "main" {
+ name       = "dareit"
+ database_version = "POSTGRES_14"
+ region      = "us-central1"
+
+ settings {
+  # Second-generation instance tiers are based on the machine
+  # type. See argument reference below.
+  tier = "db-f1-micro"
+ }
+}
+
+resource "google_sql_user" "users" {
+ name   = "dareit_user"
+ instance = google_sql_database_instance.main.name
+ password = "**********"
+}
