7f3f2441a9bca1961c22867acc4a9410222f3070
Magda Pich <72314614+magdap1601@users.noreply.github.com>
2023-05-09 20:12:10 +0200

    feat: add variable.tf file with varaibles

--- task_6/main.tf
+++ task_6/main.tf
@@ -1,53 +1,53 @@
 resource "google_storage_bucket_access_control" "public_rule" {
- bucket = google_storage_bucket.bucket.name
- role  = "READER"
- entity = "allUsers"
+  bucket = var.bucket_name
+  role   = "READER"
+  entity = var.bucket_access_control
 }
 
 resource "google_storage_bucket" "bucket" {
- name   = "bucket-created-by-terraform"
- location = "US"
+  name     = var.bucket_name
+  location = var.bucket_location
 }
 
 resource "google_compute_instance" "dare-id-vm" {
- name     = "dareit-vm-tf"
- machine_type = "e2-medium"
- zone     = "us-central1-a"
+  name         = var.compute_instance_name
+  machine_type = var.compute_instance_machine_type
+  zone         = var.compute_instance_zone
 
- tags = ["dareit"]
+  tags = ["dareit"]
 
- boot_disk {
-  initialize_params {
-   image = "debian-cloud/debian-11"
-   labels = {
-    managed_by_terraform = "true"
-   }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        managed_by_terraform = "true"
+      }
+    }
   }
- }
 
- network_interface {
-  network = "default"
+  network_interface {
+    network = "default"
 
-  access_config {
-   // Ephemeral public IP
+    access_config {
+      // Ephemeral public IP
+    }
   }
- }
 }
 
 resource "google_sql_database_instance" "main" {
- name       = "dareit"
- database_version = "POSTGRES_14"
- region      = "us-central1"
+  name             = var.sq_databes_name
+  database_version = var.sq_databes_version
+  region           = var.project_region
 
- settings {
-  # Second-generation instance tiers are based on the machine
-  # type. See argument reference below.
-  tier = "db-f1-micro"
- }
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
 }
 
 resource "google_sql_user" "users" {
- name   = "dareit_user"
- instance = google_sql_database_instance.main.name
- password = "**********"
+  name     = var.sql_databes_users_name
+  instance = google_sql_database_instance.main.name
+  password = "**********"
 }
---
+++ task_6/variables.tf
@@ -0,0 +1,73 @@
+variable "project" {
+  description = "The project ID to host the site in."
+  type        = string
+}
+
+variable "project_region" {
+  description = "The region is location for your resources to be created in."
+  type        = string
+  default     = "us-central1"
+}
+
+variable "project_zone" {
+  description = "The zone is location for your resources to be created in."
+  type        = string
+  default     = "us-central1-c"
+}
+
+#bucket
+variable "bucket_name" {
+  description = "Name for created bucket."
+  type        = string
+  default     = "bucket-created-by-terraform"
+}
+
+variable "bucket_location" {
+  description = "Bucket location."
+  type        = string
+  default     = "US"
+}
+
+variable "bucket_access_control" {
+  description = "Define who have acces for created bucket."
+  type        = string
+  default     = "allUsers"
+}
+
+#compute instance
+variable "compute_instance_name" {
+  description = "Name for created instance."
+  type        = string
+  default     = "dareit-vm-tf"
+}
+
+variable "compute_instance_zone" {
+  description = "The zone is location for your resources to be created in."
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "compute_instance_machine_type" {
+  description = "A predefine machine type that you can use when you create instance."
+  type        = string
+  default     = "us-central1-a"
+}
+
+#sql database
+variable "sql_databes_name" {
+  description = "Name for created instance."
+  type        = string
+  default     = "dareit"
+}
+
+variable "sql_databes_version" {
+  description = "Version for created database."
+  type        = string
+  default     = "POSTGRES_14"
+}
+
+variable "sql_databes_users_name" {
+  description = "Version for created database."
+  type        = string
+  default     = "dareit_user"
+}
